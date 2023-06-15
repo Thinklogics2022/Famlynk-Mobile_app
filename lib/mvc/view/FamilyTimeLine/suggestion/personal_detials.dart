9b1fe8530e2da5d8f105de89643248f70d6b88c7
paramporulfoundation <98575536+GokulPLGN@users.noreply.github.com>
2023-06-15 14:13:06 +0530

    Add Member UI

--- lib/mvc/view/FamilyTimeLine/suggestion/personal_detials.dart
+++ lib/mvc/view/FamilyTimeLine/suggestion/personal_detials.dart
@@ -1,27 +1,38 @@
-import 'package:famlynk_version1/mvc/model/suggestionModel.dart';
-import 'package:famlynk_version1/mvc/view/FamilyTimeLine/addmember/addMember.dart';
+import 'package:famlynk_version1/mvc/controller/dropDown.dart';
+import 'package:famlynk_version1/mvc/view/FamilyTimeLine/familyList/famList.dart';
+
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
-  // final FriendSuggestion friendSuggestion;
+class UserDetailsPage extends StatefulWidget {
+  @override
+  _UserDetailsPageState createState() => _UserDetailsPageState();
+}
 
-  DetailScreen();
+class _UserDetailsPageState extends State<UserDetailsPage> {
+  String dropdownValue1 = 'Select Relation';
+  late String selectedCity;
+  Map<String, dynamic> userDetails = {};
+
+  @override
+  void initState() {
+    super.initState();
+    // fetchUserDetails();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Friend Details'),
+        title: Text('User Details'),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 40),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 CircleAvatar(
                   radius: 60,
@@ -30,68 +41,90 @@ class DetailScreen extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(width: 10),
-                      Text(
-                        "Names",
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      SizedBox(width: 10),
-                      Text(
-                        "Gender",
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                   children: [
-                      SizedBox(width: 10),
-                      Text(
-                        "D-O-B",
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
-                  ), 
-                  SizedBox(height: 20),
-                  Row(
-                   children: [
-                      SizedBox(width: 10),
-                      Text(
-                        "Email",
-                        style: TextStyle(
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
-                  ), 
-                ],
+            SizedBox(height: 70),
+            Text(
+              'Name: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8.0),
+            Divider(thickness: 2),
+            SizedBox(height: 8.0),
+            Text(
+              'Gender: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8.0),
+            Divider(thickness: 2),
+            SizedBox(height: 8.0),
+            Text(
+              'Date of Birth: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8.0),
+            Divider(thickness: 2),
+            SizedBox(height: 8.0),
+            Text(
+              'Email: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8.0),
+            Divider(thickness: 2),
+            SizedBox(height: 8.0),
+            Text(
+              'Address: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8.0),
+            Divider(thickness: 2),
+            SizedBox(height: 8.0),
+            Text(
+              'HomeTown: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8.0),
+            Divider(thickness: 2),
+            SizedBox(height: 8.0),
+            Container(
+              child: DropdownButtonFormField(
+                decoration: InputDecoration(
+                    // icon: Icon(Icons.people),
+                    // enabledBorder: const OutlineInputBorder(
+                    //   borderSide: BorderSide(color: Colors.white),
+                    // ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    fillColor: Colors.grey.shade200,
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[500])),
+                dropdownColor: Color.fromARGB(255, 255, 255, 255),
+                value: dropdownValue1,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue1 = newValue!;
+                  });
+                },
+                items: relation.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  );
+                }).toList(),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => AddMember()));
-              },
-              child: Text(
-                "Add to Family",
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FamilyList()),
+                  );
+                },
+                child: Text("Add to Family"),
               ),
             ),
           ],
